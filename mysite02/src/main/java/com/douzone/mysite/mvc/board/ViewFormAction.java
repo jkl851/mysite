7696ac99db7820a594b344af26278f0f59fb56ec
jkl851 <jkl851@naver.com>
2021-10-20 10:25:52 +0900

    mysite03 #guestbook

--- mysite02/src/main/java/com/douzone/mysite/mvc/board/ViewFormAction.java
+++ mysite02/src/main/java/com/douzone/mysite/mvc/board/ViewFormAction.java
@@ -3,6 +3,7 @@ package com.douzone.mysite.mvc.board;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,17 +13,36 @@ import com.douzone.web.mvc.Action;
 import com.douzone.web.util.MvcUtil;
 
 public class ViewFormAction implements Action {
-
+	private Boolean visitedCheck = false;
+	
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Long contentNo = Long.parseLong(request.getParameter("no"));
+		Cookie[] cookies  = request.getCookies(); //hit(조회수) 조작 방지 
 		
+		for(Cookie cookie : cookies) {
+			if(cookie.getName().equals("visited" + contentNo)) {
+				visitedCheck = true;
+				}
+			}
 		
-		BoardVo vo = new BoardDao().getTitleContent(contentNo);
-		new BoardDao().hit(vo.getHit(), contentNo);
-		
-		request.setAttribute("vo", vo);
-		MvcUtil.forward("board/view", request, response);
+		if (visitedCheck) {
+			BoardVo vo = new BoardDao().getTitleContent(contentNo);
+			
+			request.setAttribute("vo", vo);
+			MvcUtil.forward("board/view", request, response);
+			
+		} else {
+			BoardVo vo = new BoardDao().getTitleContent(contentNo);
+			new BoardDao().hit(vo.getHit(), contentNo);
+			
+
+			Cookie vCookie = new Cookie("visited" + contentNo, "1");
+			vCookie.setMaxAge(60); //60초
+			response.addCookie(vCookie);
+			request.setAttribute("vo", vo);
+			MvcUtil.forward("board/view", request, response);
+		}
 	}
 
 }
