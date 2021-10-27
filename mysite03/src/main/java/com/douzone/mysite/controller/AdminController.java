d38cf5b50b482d8f5c8273da12b02a1ca5abd91e
jkl851 <jkl851@naver.com>
2021-10-27 17:51:46 +0900

    #Admin

--- mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/AdminController.java
@@ -1,19 +1,59 @@
 package com.douzone.mysite.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.ServletContext;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.douzone.mysite.exception.GalleryServiceException;
 import com.douzone.mysite.security.Auth;
+import com.douzone.mysite.service.AdminService;
+import com.douzone.mysite.vo.SiteVo;
 
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
+	private static final Log LOGGER = LogFactory.getLog(AdminController.class);
+	
+	@Autowired
+	ServletContext servletContext;
+	
+	@Autowired
+	private AdminService adminService;  
 	
 	@Auth(role="ADMIN")
 	@RequestMapping("")
-	public String main() {
-		
+	public String main(Model model) {
+		SiteVo siteVo = adminService.getSite();
+		model.addAttribute("siteVo", siteVo);
 		return "admin/main";
+	}
+	
+	@Auth(role="ADMIN")
+	@RequestMapping(value="/update", method=RequestMethod.POST)
+	public String upload(
+		SiteVo siteVo,
+		@RequestParam("file") MultipartFile file) {
+		
+		try {
+			String url = adminService.saveImage(file);
+			siteVo.setProfile(url);
+		} catch (GalleryServiceException e) {
+			LOGGER.info(e);
+		}
+		
+		adminService.UpdateSite(siteVo);
+		
+		servletContext.setAttribute("siteVo", siteVo);
+		
+		return "redirect:/admin";
 	}
 	
 	@Auth(role="ADMIN")
--- mysite03/src/main/webapp/WEB-INF/views/includes/navigation.jsp
+++ mysite03/src/main/webapp/WEB-INF/views/includes/navigation.jsp
@@ -5,9 +5,19 @@
     pageEncoding="UTF-8"%>
 		<div id="navigation">
 			<ul>
+			<c:choose>
+				<c:when test="${authUser.role == 'ADMIN' }">
+				<li><a href="${pageContext.request.contextPath }/admin">관리자페이지</a></li>
+				<li><a href="${pageContext.request.contextPath }/guestbook">방명록</a></li>
+				<li><a href="${pageContext.request.contextPath }/board">게시판</a></li>
+				<li><a href="${pageContext.request.contextPath }/gallery">갤러리</a></li>
+				</c:when>
+				<c:otherwise>
 				<li><a href="${pageContext.request.contextPath }">김민철</a></li>
 				<li><a href="${pageContext.request.contextPath }/guestbook">방명록</a></li>
 				<li><a href="${pageContext.request.contextPath }/board">게시판</a></li>
 				<li><a href="${pageContext.request.contextPath }/gallery">갤러리</a></li>
+				</c:otherwise>
+			</c:choose>
 			</ul>
 		</div>
